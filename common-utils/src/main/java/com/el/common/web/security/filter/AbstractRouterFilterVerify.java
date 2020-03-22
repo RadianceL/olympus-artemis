bf210ce83475d2ede463c01481c77b19fc3d9336
eddie <931305033@qq.com>
2020-03-22 10:34:59 +0800

    update: 新增拦截器

--- common-utils/src/main/java/com/el/common/web/security/filter/AbstractRouterFilterVerify.java
+++ common-utils/src/main/java/com/el/common/web/security/filter/AbstractRouterFilterVerify.java
@@ -10,20 +10,15 @@ import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
 import org.springframework.core.io.buffer.DataBuffer;
-import org.springframework.core.io.buffer.DataBufferUtils;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.nio.CharBuffer;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * 路由过滤器
