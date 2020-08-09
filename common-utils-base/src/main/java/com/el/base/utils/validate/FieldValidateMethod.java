65e3f067a82ac07504c8d1478ffeb931adabbe1f
eddie.lys <radiance0621@gmail.com>
2020-08-10 01:52:24 +0800

    update: 新增字段校验工具

--- common-utils-base/src/main/java/com/el/base/utils/validate/FieldValidateMethod.java
+++ common-utils-base/src/main/java/com/el/base/utils/validate/FieldValidateMethod.java
@@ -3,8 +3,6 @@ package com.el.base.utils.validate;
 import com.google.common.annotations.Beta;
 import org.apache.commons.lang3.StringUtils;
 
-import java.util.Objects;
-
 /**
  * 成员校验方法 <br/>
  * since 2020/8/10
