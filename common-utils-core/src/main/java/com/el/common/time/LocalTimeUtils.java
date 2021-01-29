1067430ac9279f9d3c7bb679603f1ed4adfd8e59
eddie.lys <liyingshi@liuheqinqiang.com>
2021-01-29 16:18:38 +0800

    update: clone input stream

--- common-utils-core/src/main/java/com/el/common/time/LocalTimeUtils.java
+++ common-utils-core/src/main/java/com/el/common/time/LocalTimeUtils.java
@@ -259,6 +259,30 @@ public class LocalTimeUtils {
     }
 
     /**
+     * 判断当前时间是否在[startTime, endTime]区间，注意时间格式要一致
+     *
+     * @param nowTime 当前时间
+     * @param startTime 开始时间
+     * @param endTime 结束时间
+     */
+    public static boolean isEffectiveDate(Date nowTime, Date startTime, Date endTime) {
+        if (nowTime.getTime() == startTime.getTime()
+                || nowTime.getTime() == endTime.getTime()) {
+            return true;
+        }
+        Calendar date = Calendar.getInstance();
+        date.setTime(nowTime);
+
+        Calendar begin = Calendar.getInstance();
+        begin.setTime(startTime);
+
+        Calendar end = Calendar.getInstance();
+        end.setTime(endTime);
+
+        return date.after(begin) && date.before(end);
+    }
+
+    /**
      * @return 返回指定月份的第一天的0分0秒
      */
     public static Date getFirstMonthOfDate() {
