64b2b9b0d9889b4be38bc130afb2f85d80e2778c
eddie.lys <ysh.li@misumi.sh.cn>
2022-11-02 15:40:11 +0800

    update: redis锁

--- olympus-artemis-core/src/main/java/com/olympus/common/web/lock/RedisDistributedLock.java
+++ olympus-artemis-core/src/main/java/com/olympus/common/web/lock/RedisDistributedLock.java
@@ -1,10 +1,11 @@
 package com.olympus.common.web.lock;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.script.DefaultRedisScript;
 
-import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
@@ -15,23 +16,20 @@ import java.util.concurrent.TimeUnit;
  * @author eddie
  */
 @Slf4j
-public class RedisDistributedLock implements AutoCloseable {
+public class RedisDistributedLock {
 
     /**
      * REDIS操作管理器
      */
     private final RedisTemplate<String, String> redisTemplate;
-
     /**
      * REDIS锁
      */
-    private static final String LOCK_KEY = "LX-TRADE-CENTER:REDIS-LOCK:LOCK_TRADE";
-
+    private static final String LOCK_KEY = "LX-TRADE-CENTER:REDIS-LOCK:LOCK_TRADE:";
     /**
      * 随机数
      */
     private static final String LOCK_VALUE = UUID.randomUUID().toString();
-
     /**
      * 成功的返回值
      */
@@ -41,20 +39,40 @@ public class RedisDistributedLock implements AutoCloseable {
         this.redisTemplate = redisTemplate;
     }
 
-    public Boolean lock() {
-        long timeout = 20;
-        Boolean lockIfSuccess = redisTemplate.opsForValue().setIfAbsent(LOCK_KEY, LOCK_VALUE, timeout, TimeUnit.SECONDS);
+    public Boolean lock(String lockKey, Long timeout) {
+        return lock(lockKey, LOCK_VALUE, timeout);
+    }
+
+    public Boolean lock(String lockKey, String lockValue, Long timeout) {
+        if (StringUtils.isBlank(lockKey)) {
+            return false;
+        }
+        lockKey = LOCK_KEY.concat(lockKey);
+        if (Objects.isNull(timeout)) {
+            timeout = 20L;
+        }
+        Boolean lockIfSuccess = redisTemplate.opsForValue()
+                .setIfAbsent(lockKey, lockValue, timeout, TimeUnit.SECONDS);
         if (Objects.isNull(lockIfSuccess)){
             return false;
         }
         return lockIfSuccess;
     }
 
-    @Override
-    public void close() {
+    public Boolean releaseLock(String lockKey) {
+        return releaseLock(lockKey, LOCK_VALUE);
+    }
+
+    public Boolean releaseLock(String lockKey, String lockValue) {
+        if (StringUtils.isBlank(lockKey)) {
+            return false;
+        }
+        lockKey = LOCK_KEY.concat(lockKey);
+
         DefaultRedisScript<Long> defaultRedisScript = new DefaultRedisScript<>();
         defaultRedisScript.setResultType(Long.class);
-        defaultRedisScript.setScriptText("if redis.call('get', KEYS[1]) == KEYS[2] then return redis.call('del', KEYS[1]) else return 0 end");
-        redisTemplate.execute(defaultRedisScript, Arrays.asList(LOCK_KEY, LOCK_VALUE));
+        defaultRedisScript.setScriptText("if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end");
+        Long result = redisTemplate.execute(defaultRedisScript, List.of(lockKey), lockValue);
+        return Objects.nonNull(result) && RELEASE_SUCCESS.equals(result);
     }
 }
